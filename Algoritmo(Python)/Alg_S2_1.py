45c16033e865666022a78e4e3abb10a730be1735
Daniel Conte <182736@upf.br>
2020-04-13 00:05:18 -0300

    atualizando

---
+++ Algoritmo(Python)/Alg_S2_1.py
@@ -0,0 +1,8 @@
+#input
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+#process
+soma = (num1 + num2)
+mult = (soma * num1)
+#output
+print("O resultado é: {0}".format(mult))
